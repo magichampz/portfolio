d0200a6eedb8f773e6ddd8a7b16351b189aafa8b
magichampz <91732309+magichampz@users.noreply.github.com>
2024-02-28 11:18:22 +0000

    initial webpage

---
+++ index.js
@@ -0,0 +1,58 @@
+/* -----------------------------------------
+  Have focus outline only for keyboard users 
+ ---------------------------------------- */
+
+const handleFirstTab = (e) => {
+  if(e.key === 'Tab') {
+    document.body.classList.add('user-is-tabbing')
+
+    window.removeEventListener('keydown', handleFirstTab)
+    window.addEventListener('mousedown', handleMouseDownOnce)
+  }
+
+}
+
+const handleMouseDownOnce = () => {
+  document.body.classList.remove('user-is-tabbing')
+
+  window.removeEventListener('mousedown', handleMouseDownOnce)
+  window.addEventListener('keydown', handleFirstTab)
+}
+
+window.addEventListener('keydown', handleFirstTab)
+
+const backToTopButton = document.querySelector(".back-to-top");
+let isBackToTopRendered = false;
+
+let alterStyles = (isBackToTopRendered) => {
+  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? "scale(1)"
+    : "scale(0)";
+};
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+$(document).ready(function(){
+  $('#portfolio-flters li').click(function(){
+    var filter = $(this).data('filter');
+    $('#portfolio-flters li').removeClass('filter-active');
+    $(this).addClass('filter-active');
+    
+    if(filter == '*'){
+      $('.work__box').show();
+    } else {
+      $('.work__box').hide();
+      $(filter).show();
+    }
+  });
+});
